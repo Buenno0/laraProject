3049e3d27109f4e1f34a20ecfb9b1ec6faf43817
Bueno <bueno.mateus@aluno.ifsp.edu.br>
2024-08-11 13:14:04 -0300

    feat: Add routes for posts and users resources
    This commit adds routes for the `posts` and `users` resources in the `web.php` file. It also includes a route to clear the `User` table. This will allow users to perform CRUD operations on posts and users, and also clear the user table when needed.

--- laragonTeste/routes/web.php
+++ laragonTeste/routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::resource('posts', PostController::class);
+Route::resource('users', UserController::class);
+
+Route::get('/clear', function () {
+    User::truncate();
+    return 'Tabela limpa com sucesso!';
+});
 
 Route::get('/', function () {
     return view('welcome');
